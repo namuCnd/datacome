0afe3a2a47880285d4b475b5e93c2994524b29d6
jinu <jinu@DESKTOP-CG12UKI>
2018-08-11 15:20:11 +0900

    게시판 페이징

--- datacome/src/main/java/com/namucnd/main/controller/MainController.java
+++ datacome/src/main/java/com/namucnd/main/controller/MainController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.namucnd.user.domain.Users;
 import com.namucnd.user.service.UsersService;
