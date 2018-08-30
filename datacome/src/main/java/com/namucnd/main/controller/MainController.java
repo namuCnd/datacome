effc258bea1e487f908330e64dac04c7838b4a41
jinu <jinu@DESKTOP-E0EOKP1>
2018-08-30 18:59:47 +0900

    아이피 저장

--- datacome/src/main/java/com/namucnd/main/controller/MainController.java
+++ datacome/src/main/java/com/namucnd/main/controller/MainController.java
@@ -10,24 +10,40 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.namucnd.monitor.domain.MonitorIp;
+import com.namucnd.monitor.service.MonitorIpService;
 import com.namucnd.user.domain.Users;
-import com.namucnd.user.service.UsersService;
 
 @Controller
 public class MainController {
-	
+
 	// log사용을 위해 선언
 	Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
-	UsersService usersService;
-	
-	@RequestMapping(value="/main/main.do")
-	public String main(HttpServletRequest request, ModelMap model, HttpSession session,
-			Users users) throws Exception {
+	MonitorIpService monitorIpService;
+
+	@RequestMapping(value = "/main/main.do")
+	public String main(HttpServletRequest request, ModelMap model, HttpSession session, Users users) throws Exception {
+
+		String ip = request.getHeader("X-FORWARDED-FOR");
+
+		if (ip == null || ip.length() == 0) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+
+		if (ip == null || ip.length() == 0) {
+			ip = request.getHeader("WL-Proxy-Client-IP"); // 웹로직
+		}
+
+		if (ip == null || ip.length() == 0) {
+			ip = request.getRemoteAddr();
+		}
+		MonitorIp monitorIp = new MonitorIp();
+		monitorIp.setReq_ip(ip);
 		
-		System.out.println("users:"+ users.toString());
-		
+		monitorIpService.insertReqIp(monitorIp);
+
 		return "/home.default";
 	}
 
